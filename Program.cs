a14064831d85509dd2c4c882a02513b20b489e87
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-15 16:55:28 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercise asks for the longest word in each of these two sentences, output 1 result per line
+            "Some line with text"
+            "Another line"*/
+
+            string sentence = "I have a dream.";
+            int sentenceLength = sentence.Length;
+
+            string[] sentenceArray = sentence.Split(' ');
+            //splitting on the charecter SPACE
+
+            string longWord = "";
+            foreach (string word in sentenceArray) 
+            
+            {
+                int wordLength = word.Length;
+                int longWordLength = longWord.Length;
+
+                if (wordLength > longWordLength)
+                {
+                    longWordLength = wordLength;
+                    longWord = word;
+                }
+                
+
+                //For each word we need to count the chars and find the longest word
+            }
+            Console.WriteLine(longWord);
+        }
+    }
+}
